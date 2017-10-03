0b24e944c98e1ff2e8673c4952046ca798ef47d0
Santosh Pant <santos.xwolverine@gmail.com>
2017-10-03 08:12:26 +0530

    Arena

--- tetris.js
+++ tetris.js
@@ -1,15 +1,90 @@
+function createMatrix(w, h){
+    const matrix2 = [];
+    while(h--){
+    matrix2.push(Array(w).fill(0))
+    }
+}
+createMatrix(5, 7)
+/*
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 context.scale(20, 20);
-context.fillStyle = "#000";
-context.fillRect(0, 0, canvas.width, canvas.height);
+
 
 const matrix = [
     [0, 0, 0],
     [1, 1, 1],
-    [0, 1, 0]
+    [1, 0, 0]
 ];
 
+function draw(){
+    context.fillStyle = `peachpuff`;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset){
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                context.fillStyle = "red";
+                context.fillRect(x + offset.x, y + offset.y, 1, 1);
+            }
+        })
+    })
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+function update(time = 0){
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    dropCounter += deltaTime
+    if(dropCounter > dropInterval) {
+        player.pos.y++;
+        dropCounter = 0;
+    }
+    draw();
+    requestAnimationFrame(update);
+}
+
+function playerDrop(){
+    player.pos.y++;
+    dropCounter = 0;
+}
+const player = {
+    pos : {x:5, y:5},
+    matrix : matrix
+}
+
+document.addEventListener('keydown', fire =>{
+    if(fire.keyCode == 40){
+         playerDrop();
+    }
+
+    if(fire.keyCode == 39){
+        player.pos.x++;
+    }
+
+    if(fire.keyCode == 37){
+        player.pos.x--;
+    }
+    
+});
+
+
+function createMatrixw(w, h){
+    const matrix = [];
+    
+
+}
+
+update();
+*/
+
+/*
 function draw() {
     context.fillStyle = "#000";
     context.fillRect(0, 0, canvas.width, canvas.height);
@@ -81,5 +156,4 @@ document.addEventListener('keydown', event => {
 })
 update();
 
-
-
+*/
